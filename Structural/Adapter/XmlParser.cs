78a237e21333ecadb4eea19fe0fb293ba790f73a
Vangel <venonomer7@abv.bg>
2024-05-27 12:37:32 +0300

    Adding adapter pattern

---
+++ Structural/Adapter/XmlParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DesignPatterns.Structural.Adapter
+{
+    public class XmlParser<T> : IXmlParser<T>
+    {
+        private readonly XmlSerializer _serializer;
+
+        public XmlParser()
+        {
+            _serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(typeof(T).Name.ToLower()));
+        }
+
+        public string ConvertToXml(T obj)
+        {
+            using StringWriter textWriter = new StringWriter();
+            _serializer.Serialize(textWriter, obj);
+
+            return textWriter.ToString();
+        }
+
+        public T Parse(string data)
+        {
+            using TextReader reader = new StringReader(data);
+            return (T)_serializer.Deserialize(reader);
+        }
+    }
+}
---
+++ Structural/Adapter/XmlToJsonAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DesignPatterns.Structural.Adapter
+{
+    public class XmlToJsonAdapter<T> : IJsonParser<T>
+    {
+        public string ConvertToJson(T obj)
+        {
+            IJsonParser<T> jsonParser = new JsonParser<T>();
+            return jsonParser.ConvertToJson(data);
+
+        }
+
+        public T Parse(string data)
+        {
+            IXmlParser<T> xmlParser = new XmlParser<T>();
+            return xmlParser.Parse(data);
+        }
+    }
+}

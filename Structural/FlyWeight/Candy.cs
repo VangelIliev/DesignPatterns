205f5632cb31c9772e06ae45abe139851c44896d
Vangel <venonomer7@abv.bg>
2024-05-30 15:59:47 +0300

    Adding flyweight pattern

---
+++ Structural/FlyWeight/Candy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.FlyWeight
+{
+    public class Candy
+    {
+        public string Colour { get; private set; }
+
+        public string Shape { get; private set; }
+
+        public Candy(string color, string shape)
+        {
+            Colour = color;
+            Shape = shape;
+        }
+    }
+}
---
+++ Structural/FlyWeight/CandyFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.FlyWeight
+{
+    public class CandyFactory
+    {
+        private Dictionary<string, Candy> _candies = new();
+        // the pattern is about saving memory
+        public Candy GetCandy(string color, string shape)
+        {
+            //create a key from the passed strings
+
+            string key = $"{color}_{shape}";
+
+            //add a candy instance for the key, if don't exists in the dictionary
+            if(!_candies.ContainsKey(key))
+            {
+                _candies[key] = new Candy(color, shape);
+            }
+            //Otherwise return  the cached candy
+            return _candies[key];
+        }
+    }
+}
